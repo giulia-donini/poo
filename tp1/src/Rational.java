d97a5f7b884352701d3906d5e13796aa5b530a2f
giulia <giuliadoninib@gmail.com>
2024-09-24 20:47:58 +0200

    tp1 fini

--- tp1/src/Rational.java
+++ tp1/src/Rational.java
@@ -1,11 +1,58 @@
 public class Rational {
    int num;
    int denom;
+
    public Rational(int num, int denom) {
+       if (denom == 0){
+           throw new IllegalArgumentException("denominator cannot be zero");
+       }
        this.num = num;
        this.denom = denom;
+       this.simplify();
    }
+
    public String toString() {
        return num + "/" + denom;
    }
+
+   public void mult(Rational other){
+       this.num *= other.num;
+       this.denom *= other.denom;
+
+       this.simplify();
+   }
+
+    public void add(Rational other){
+        this.num = this.num * other.denom + other.num * this.denom;
+        this.denom = this.denom * other.denom;
+
+        this.simplify();
+    }
+
+    public int pgcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return pgcd(b, a % b);
+        }
+    }
+
+    public void simplify(){
+        int pgcdValue = pgcd(Math.abs(this.num), Math.abs(this.denom));
+        this.num /= pgcdValue;
+        this.denom /= pgcdValue;
+
+        if (denom < 0) {
+            num = -num;
+            denom = -denom;
+        }
+    }
+
+    public static Rational mult(Rational a, Rational b) {
+        Rational c = new Rational(a.num * b.num, a.denom * b.denom);
+        c.simplify();
+        return c;
+    }
+
 }
+
--- tp1/src/TestRational.java
+++ tp1/src/TestRational.java
@@ -1,6 +1,27 @@
 public class TestRational {
     public static void main(String[] args) {
-        Rational rational = new Rational(3,2);
-        System.out.println(rational);
+        // prints rational 1 == 3/2
+        Rational r1 = new Rational(3,2);
+        System.out.println(r1);
+
+       /* forced error to test when denom == 0
+        Rational rational0 = new Rational(3,0);
+        System.out.println(rational0); */
+
+        // prints rational 1 == 1/3
+        Rational r2 = new Rational(1,3);
+        System.out.println(r2);
+
+        // prints rational 1 == rational 1 * rational 2 == 3/6
+        r1.mult(r2);
+        System.out.println(r1);
+
+        // prints rational 1 == rational 1 + rational 2 == 15/18
+        r1.add(r2);
+        System.out.println(r1);
+
+        Rational r3 = Rational.mult(r1,r2);
+        System.out.println(r3);
+
     }
 }
